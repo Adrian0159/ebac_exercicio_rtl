2e1b6a4f65c633733dbfbf65f477f01bad740aa9
Adriano Gonçalves da Silva <88063170+Adrian0159@users.noreply.github.com>
2023-10-24 13:57:40 -0300

    adicionando teste

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,21 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente Post", () => {
+  test("Deve renderizar o componente corretamente", () => {
+    render(<Post />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("Deve adicionar um comentário", () => {
+    render(<PostComment />);
+    fireEvent.change(screen.getByTestId("campo-comentar"), {
+      target: {
+        value: "Teste",
+      },
     });
+    fireEvent.click(screen.getByTestId("btn-comentar"));
+    expect(screen.getByText("Teste")).toBeInTheDocument();
+  });
 });
