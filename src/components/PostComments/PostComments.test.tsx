1c8bcfc382445295afd848bab8dadad66ecd72b8
Adriano Gonçalves da Silva <88063170+Adrian0159@users.noreply.github.com>
2023-10-25 09:12:27 -0300

    adicinando mais um comentario

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -2,14 +2,25 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import PostComment from ".";
 
 describe("Teste para o componente Post", () => {
-  test("Deve adicionar um comentário", () => {
+  test('Deve adicionar "Primeiro comentário"', () => {
     render(<PostComment />);
     fireEvent.change(screen.getByTestId("campo-comentar"), {
       target: {
-        value: "Teste",
+        value: "Primeiro comentário",
       },
     });
     fireEvent.click(screen.getByTestId("btn-comentar"));
-    expect(screen.getByText("Teste")).toBeInTheDocument();
+    expect(screen.getByText("Primeiro comentário")).toBeInTheDocument();
+  });
+
+  test('Deve adicionar "Segundo comentário"', () => {
+    render(<PostComment />);
+    fireEvent.change(screen.getByTestId("campo-comentar"), {
+      target: {
+        value: "Segundo comentário",
+      },
+    });
+    fireEvent.click(screen.getByTestId("btn-comentar"));
+    expect(screen.getByText("Segundo comentário")).toBeInTheDocument();
   });
 });
